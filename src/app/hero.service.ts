f86a7465dde4767389c77de9dbd1e1c1a6e18d9c
Michael F. Rieser <rieser.20@osu.edu>
2019-03-04 13:22:15 -0500

    async heroes from svc and messages component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs'; // use of() to simulate HttpClient RxJS Observable object
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root' // Angular creates single, shared instance of service and injects into any class that asks for it
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES); // emits the array of mock heroes
   }
 
 }
